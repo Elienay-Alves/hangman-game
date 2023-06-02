31cd9f2199623b29b064ba41f15031d42ca1d6c5
Elienay Alves <elienay8486@gmail.com>
2023-06-02 16:25:33 -0300

    feat(user_interaction.py): update verification of letter

--- src/user_interaction.py
+++ src/user_interaction.py
@@ -1,5 +1,11 @@
+import re
+
+
 def guess():
     guessed_letter = input("Guess a letter: ")
+
+    if re.search(r"[\d+\-*/%&$#@!~^]", guessed_letter):
+        raise ValueError("You must type a letter")
 
     if len(guessed_letter) == 0:
         raise ValueError("You must type a letter")
