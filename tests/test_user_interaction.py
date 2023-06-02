94aae8d099a538f809dd3ad510968ba66c4fef3d
Elienay Alves <elienay8486@gmail.com>
2023-06-02 15:29:26 -0300

    test(test_user_interaction.py): add test to assert if there is a ValueError when input is umpty

--- tests/test_user_interaction.py
+++ tests/test_user_interaction.py
@@ -19,6 +19,22 @@ def test_if_input_is_not_empty():
 
     with patch("builtins.input", return_value=simulated_input):
         guessed_letter = guess()
+        assert len(guessed_letter) > 0, "The input should contain a letter"
+
+
+def test_if_length_of_input_is_one():
+    simulated_input = "l"
+
+    with patch("builtins.input", return_value=simulated_input):
+        guessed_letter = guess()
         assert (
-            not len(guessed_letter) == 0
-        ), "The input should contain a letter"
+            len(guessed_letter) == 1
+        ), "The input should contain only one letter"
+
+
+def test_if_input_raises_ValueError_when_empty():
+    simulated_input = ""
+
+    with patch("builtins.input", return_value=simulated_input):
+        with pytest.raises(ValueError, match="You must type a letter"):
+            guess()
