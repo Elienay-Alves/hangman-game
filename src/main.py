7c9ad88f60600189e1e63c205407df36268906cf
Elienay Alves <elienay8486@gmail.com>
2023-06-02 17:04:46 -0300

    feat(main.py): create logic to show dunder and right and wrong letters

--- src/main.py
+++ src/main.py
@@ -8,8 +8,13 @@ CHOSEN_WORD = random.choice(word_list)
 user_guess = guess()
 
 print(user_guess)
-for letter in CHOSEN_WORD:
+word_dunder = ["_"] * len(CHOSEN_WORD)
+
+for index, letter in enumerate(CHOSEN_WORD):
     if letter == user_guess:
         print("right")
+        word_dunder[index] = letter
     else:
         print("wrong")
+
+print("".join(word_dunder))
