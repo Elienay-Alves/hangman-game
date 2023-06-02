0181d9be518c62206df895950cc07372ceb7f426
Elienay Alves <elienay8486@gmail.com>
2023-06-02 13:14:32 -0300

    test(test_user_interaction.py): test if input is a string

--- tests/test_user_interaction.py
+++ tests/test_user_interaction.py
@@ -4,9 +4,9 @@ from unittest.mock import patch
 from src.user_interaction import guess
 
 
-def test_if_guess_uses_input_correctly():
+def test_input_is_string():
     simulated_input = "l"
 
     with patch("builtins.input", return_value=simulated_input):
-        guessed_letter = guess
-        assert guessed_letter == simulated_input, "Guess dont have a input"
+        guessed_letter = guess()
+        assert isinstance(guessed_letter, str), "The input is not a string"
