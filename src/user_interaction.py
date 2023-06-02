aa14f0f160dd64b2cc1de85ecd08fe1b892121e8
Elienay Alves <elienay8486@gmail.com>
2023-06-02 16:33:51 -0300

    feat(user_interaction.py): update user_interaction implementation

--- src/user_interaction.py
+++ src/user_interaction.py
@@ -5,7 +5,7 @@ def guess():
     guessed_letter = input("Guess a letter: ")
 
     if re.search(r"[\d+\-*/%&$#@!~^]", guessed_letter):
-        raise ValueError("You must type a letter")
+        raise ValueError("You can type only letter")
 
     if len(guessed_letter) == 0:
         raise ValueError("You must type a letter")
