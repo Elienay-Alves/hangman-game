0b0ee57f5197a0559a5a7c7adfea7f57ea9c8d12
Elienay Alves <elienay8486@gmail.com>
2023-06-02 15:10:23 -0300

    test(test_user_interaction.py): add test to empty case in input

--- tests/test_user_interaction.py
+++ tests/test_user_interaction.py
@@ -4,10 +4,21 @@ import pytest
 from src.user_interaction import guess
 
 
-def test_if_raises_TypeError_when_input_is_not_a_letter():
-    simulated_input = "#"
+def test_if_input_is_a_letter():
+    simulated_input = "l"
+
     with patch("builtins.input", return_value=simulated_input):
         guessed_letter = guess()
         assert not re.search(
             r"[\d+\-*/%&$#@!~^]", guessed_letter
         ), "The input should not contain a number, symbol or signal"
+
+
+def test_if_input_is_not_empty():
+    simulated_input = "l"
+
+    with patch("builtins.input", return_value=simulated_input):
+        guessed_letter = guess()
+        assert (
+            not len(guessed_letter) == 0
+        ), "The input should contain a letter"
